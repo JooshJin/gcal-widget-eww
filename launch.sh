ebd232991d1c44f169648783175ebd3ea402d43f
JooshJin <joshuajin0625@gmail.com>
2026-06-06 19:34:56 +0900

    updated to use transparency and highlight day of week. background layering still wip.

--- launch.sh
+++ launch.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 eww kill 2>/dev/null
 sleep 0.3
-GTK_THEME=Adwaita eww daemon
+GTK_THEME=EwwTransparent eww daemon
 sleep 5
 eww open calendar
-sleep 0.3
-xdotool search --class "Eww" windowlower
+sleep 0.5
+WID=$(xdotool search --class "Eww" | tail -1)
+wmctrl -i -r "$WID" -b remove,above
+wmctrl -i -r "$WID" -b add,below
